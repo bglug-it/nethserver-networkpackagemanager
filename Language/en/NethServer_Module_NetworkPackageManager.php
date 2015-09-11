e2cb66e0bc2075689efa1f94ff9f4ac7eed80953
enricobacis <enrico.bacis@gmail.com>
2015-09-11 20:15:59 +0200

    Add 'en' and 'it' localizations

---
+++ Language/en/NethServer_Module_NetworkPackageManager.php
@@ -0,0 +1,16 @@
+<?php 
+
+/* /usr/share/nethesis/NethServer/Language/en/NethServer_Module_NetworkPackageManager.php */
+
+$L['NetworkPackageManager_Title'] = 'Network Package Manager'; 
+$L['NetworkPackageManager_Description'] = 'Configure packages installed on clients'; 
+$L['NetworkPackageManager_Tags'] = 'network install packages client role'; 
+$L['Key_label'] = 'Role'; 
+$L['role_label'] = 'Role'; 
+$L['Addpkg_label'] = 'Install'; 
+$L['Delpkg_label'] = 'Remove'; 
+$L['Description_label'] = 'Description'; 
+$L['Confirm deletion of'] = 'Confirm deletion of'; 
+$L['Create a new role'] = 'Create a new role'; 
+$L['Update role `${0}`'] = 'Update role `${0}`'; 
+
---
+++ Language/it/NethServer_Module_NetworkPackageManager.php
@@ -0,0 +1,16 @@
+<?php 
+
+/* /usr/share/nethesis/NethServer/Language/it/NethServer_Module_NetworkPackageManager.php */
+
+$L['NetworkPackageManager_Title'] = 'Gestione Pacchetti della Rete'; 
+$L['NetworkPackageManager_Description'] = 'Configura i pacchetti installati sui pc della rete'; 
+$L['NetworkPackageManager_Tags'] = 'network install pacchetti package client role ruolo'; 
+$L['Key_label'] = 'Ruolo'; 
+$L['role_label'] = 'Ruolo'; 
+$L['Addpkg_label'] = 'Installa'; 
+$L['Delpkg_label'] = 'Rimuovi'; 
+$L['Description_label'] = 'Descrizione'; 
+$L['Confirm deletion of'] = 'Conferma la cancellazione di'; 
+$L['Create a new role'] = 'Crea un nuovo ruolo'; 
+$L['Update role `${0}`'] = 'Aggiorna il ruolo `${0}`'; 
+
